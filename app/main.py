adfc61aeaf71abb857bbbb2c2a010c9534f521e8
priyansi <priyanhiman@gmail.com>
2020-08-24 21:58:55 +0530

    fixed prediction

--- app/main.py
+++ app/main.py
@@ -2,7 +2,7 @@
 from flask import Flask, request, jsonify
 import os
 import numpy as np
-import torch.utils.py
+from torch_utils import transform_image, get_prediction
 
 app = Flask(__name__)
 
@@ -10,7 +10,7 @@ ALLOWED_EXTENSIONS = {'png', 'jpg', 'jpeg'}
 
 
 def allowed_file(filename):
-    return '.' in filename
+    return '.' in filename and filename.rsplit('.', 1)[1].lower() in ALLOWED_EXTENSIONS
 
 
 @app.route('/predict', methods=['POST'])
@@ -22,13 +22,11 @@ def predict():
         if not allowed_file(file.filename):
             return jsonify({'error': 'format not supported'})
 
-    print('here')
-
     # try:
     img_bytes = file.read()
     tensor = transform_image(img_bytes)
     prediction = get_prediction(tensor)
-    data = {'prediction': prediction}
+    data = {'prediction': prediction.tolist()}
     return jsonify(data)
 
     # except:
