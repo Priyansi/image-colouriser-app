7e7040deeb3217d50436fe827967483650ba9a2d
priyansi <priyanhiman@gmail.com>
2020-08-25 03:32:29 +0530

    added dependency

--- app/test/test.py
+++ app/test/test.py
@@ -1,6 +1,19 @@
+import json
 import requests
+import numpy as np
+from PIL import Image
+import io
+import matplotlib.pyplot as plt
 
-resp = requests.post("https://image-colouriser.herokuapp.com/predict",
-                     files={'file': open('test.jpg', 'rb')})
 
-print(resp.text)
+def predict_array(PATH):
+    resp = requests.post("https://image-colouriser.herokuapp.com/predict",
+                         files={'file': open(PATH, 'rb')})
+
+    return resp.text
+
+
+img_dict = json.loads(predict_array('test.jpg'))
+img_list = img_dict['prediction']
+img_arr = np.array(img_list)
+plt.imsave('prediction.png', img_arr)
